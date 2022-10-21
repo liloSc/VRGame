4216f5030e4ca925b3c178c590beff8b125f0682
liloSc <52287760+liloSc@users.noreply.github.com>
2022-10-21 10:07:29 +0200

    Add Background Sound

--- Assets/Scripts/WritingAssistant.cs
+++ Assets/Scripts/WritingAssistant.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class WritingAssistant : MonoBehaviour
 {
 private Text messageText;
-private string desiredText= "Introduction to our Space Game";
+private string desiredText= "Introduction to our Space Game"+ Environment.NewLine + "line two";
 
    private void Awake() {
         messageText= transform.Find("Intro_text").GetComponent<Text>();
