c8720897619bc5be43e95bc08fe2ee78af895b41
k-koleva <katerinagkoleva@gmail.com>
2022-10-23 19:13:01 +0200

    added more info saturn text

--- Assets/Scripts/HideOrShow.cs
+++ Assets/Scripts/HideOrShow.cs
@@ -7,11 +7,31 @@ public class HideOrShow : MonoBehaviour
 {
     public GameObject CanvasObj;
     public bool visible;
-    public Text txt;
+    public Text title;
     public string planetName;
+    public RawImage planetImage;
+    public Texture sourceImg;
+
+    public Text information;
+    public string planetInfo;
+    public Text diameter;
+    public string diameterString;
+    public Text distance;
+    public string distanceString;
+    public Text mass;
+    public string massString;
+    public Text orbitalPeriod;
+    public string orbitalPeriodStr;
+    public Text moons;
+    public string moonsString;
+
     public float PosX;
     public float PosY;
     public float PosZ;
+
+    public float RotX;
+    public float RotY;
+    public float RotZ;
 
 
     private void Start()
@@ -26,20 +46,34 @@ public class HideOrShow : MonoBehaviour
 
     public void show()
     {
-        setText(planetName);
-        setPosition(PosX, PosY, PosZ);
+        setText(planetName, planetInfo, diameterString, distanceString, massString, orbitalPeriodStr, moonsString);
+        setPosANDRot(PosX, PosY, PosZ, RotX, RotY, RotZ);
+        setImage(sourceImg);
         CanvasObj.SetActive(true);
     }
 
-    public void setText(string name)
+    public void setText(string name, string info, string di, string dist, string mas, string orb, string mo)
     {
-        txt.GetComponent<UnityEngine.UI.Text>().text = name;
+        title.GetComponent<UnityEngine.UI.Text>().text = name;
+        information.GetComponent<UnityEngine.UI.Text>().text = info;
+        diameter.GetComponent<UnityEngine.UI.Text>().text = di;
+        distance.GetComponent<UnityEngine.UI.Text>().text = dist;
+        mass.GetComponent<UnityEngine.UI.Text>().text = mas;
+        orbitalPeriod.GetComponent<UnityEngine.UI.Text>().text = orb;
+        moons.GetComponent<UnityEngine.UI.Text>().text = mo;
     }
 
-    public void setPosition(float x, float y, float z)
+    public void setPosANDRot(float px, float py, float pz, float rx, float ry, float rz)
     {
         Transform transform1 = CanvasObj.transform;
-        transform1.position = new Vector3(x, y, z);
+        transform1.position = new Vector3(px, py, pz);
+        transform1.rotation = new Quaternion(rx, ry, rz,0);
+    }
+
+
+    public void setImage(Texture source)
+    {
+        planetImage.texture = source;
     }
   
 }
