c8aec68bd8a2d9dddc30e6db9fc40e767bf61b58
k-koleva <katerinagkoleva@gmail.com>
2022-10-23 14:12:54 +0200

    test moving text near snapzone

--- Assets/Scripts/HideOrShow.cs
+++ Assets/Scripts/HideOrShow.cs
@@ -9,6 +9,9 @@ public class HideOrShow : MonoBehaviour
     public bool visible;
     public Text txt;
     public string planetName;
+    public float PosX;
+    public float PosY;
+    public float PosZ;
 
 
     private void Start()
@@ -24,12 +27,19 @@ public class HideOrShow : MonoBehaviour
     public void show()
     {
         setText(planetName);
+        setPosition(PosX, PosY, PosZ);
         CanvasObj.SetActive(true);
     }
 
     public void setText(string name)
     {
         txt.GetComponent<UnityEngine.UI.Text>().text = name;
+    }
+
+    public void setPosition(float x, float y, float z)
+    {
+        Transform transform1 = CanvasObj.transform;
+        transform1.position = new Vector3(x, y, z);
     }
   
 }
